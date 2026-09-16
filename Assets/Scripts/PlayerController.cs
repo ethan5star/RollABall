d4983d86c1f92669247cf05a25efa1e40352bef5
5starethan <eedward29@sandomenico.org>
2026-09-16 08:55:38 -0700

    Added Player Control Functionality.
    Updated scene, build PlayerController and CameraController Scripts, gave the Player a Player Input component.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+/************************************************************
+* COMPONENT OF: Player
+* REQUIRED DEPENDENCIES: Rigidbody Component
+* DESCRIPTION: It listens for WASD and Arrow key presses to set 
+*              vertical and horizontal directions. It uses those
+*              to push the player's Rigidbody in that direction
+*              at a preset force.
+* AUTHOR: EEdward
+* DATE WRITTEN: Sep 16 2026
+* VERSION: 1.0
+*************************************************************/
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+    // Movement fields
+    private float horizontalMovement;
+    private float verticalMovement;
+    private float force;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        horizontalMovement = 0.707f;
+        verticalMovement = 0.707f;
+        force = 4.75f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       MovePlayer(); 
+    }
+
+
+        // Moves the player
+    private void MovePlayer()
+    {
+        Vector3 direction = new Vector3(horizontalMovement, 0, verticalMovement);
+        GetComponent<Rigidbody>().AddForce(Vector3.forward * force); 
+    }
+
+}
